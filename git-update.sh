251dee71afeda791183cc5df5a2f96a3d21257fa
zhangyuan@linux_centos64-bits <love.zyb@outlook.com>
2019-04-08 00:25:05 +0800

    新增git提交脚本

---
+++ git-update.sh
@@ -0,0 +1,24 @@
+#########################################################################
+# File Name: git-update.sh
+# Author: zhangyuanbo
+# mail: pual.office@gmail.com
+# Created Time: 2019-04-07
+#########################################################################
+#!/bin/bash
+
+if [ $# -lt 0 ]
+then
+	echo '请输入提交注释'
+	exit 127
+fi
+
+if [ -z $1 ]
+then
+	echo '参数不能为空'
+	exit 126
+fi
+
+date=`date +%Y-%m-%d %H:%M`
+git add .
+git commit -m "$date $1"
+git push
