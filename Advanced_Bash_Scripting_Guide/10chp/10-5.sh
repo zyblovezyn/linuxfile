54ce76b5d47a4978510da631eed1de5ae4ff732c
zhangyuan@linux_centos64-bits <love.zyb@outlook.com>
2019-04-08 22:58:55 +0800

    提交时间:2019-04-08 22:58 注释:新增shell代码

---
+++ Advanced_Bash_Scripting_Guide/10chp/10-5.sh
@@ -0,0 +1,14 @@
+#########################################################################
+# File Name: 10-5.sh
+# Author: zhangyuanbo
+# mail: pual.office@gmail.com
+# Created Time: 2019-04-08
+#########################################################################
+#!/bin/bash
+
+for a
+do
+	echo -n "$a"
+done
+echo
+exit 0
