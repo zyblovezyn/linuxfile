cdd02f05edb918a0373a0b91db8b1e8146e6d83a
“zhangyuanbo@home” <luoyiguren@outlook.com>
2019-05-18 11:32:48 -0400

    提交时间:2019-05-18 11:32 注释:add 11-2.sh

---
+++ docker/docker.sh
@@ -0,0 +1,10 @@
+#########################################################################
+# File Name: docker.sh
+# Author: zhangyuanbo
+# mail: love.zyb@outlook.com
+# Created Time: Fri 17 May 2019 09:04:32 AM EDT
+#########################################################################
+#!/bin/bash
+
+curl -fsSL get.docker.com -o get-docker.sh
+sh get-docker.sh --mirror Aliyun
